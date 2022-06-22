a72edff9c4201db2f125450972c1cbb71cacb782
Azry <azry695@gmail.com>
2022-06-22 11:01:53 +0700

    Update Reply-cih.js

--- plugins/Reply-cih.js
+++ plugins/Reply-cih.js
@@ -1,4 +1,4 @@
-let fs = require('fs')
+import fs from 'fs'
 let handler = async (m) => {
 let stc = pick("https://telegra.ph/file/fa2bbea0f7de2575cf027.png")
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*NANI*', 'status@broadcast')
@@ -6,4 +6,4 @@ conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*NANI*', 'status@broadcast')
 handler.customPrefix = /Cih/i
 handler.command = new RegExp
 
-module.exports = handler
+export default handler
